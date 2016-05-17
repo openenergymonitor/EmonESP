450b8bd536a8bdd1b0d713ef59064c574c2f9d53
chris1howell <chris1howell@msn.com>
2016-05-17 07:57:27 -0700

    Add support for images/files in html
    Added content types for common file types and added logo image to HTML.

--- EmonESP.ino
+++ EmonESP.ino
@@ -71,6 +71,38 @@ unsigned long Timer;
 unsigned long packets_sent = 0;
 unsigned long packets_success = 0;
 
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){
+  if(path.endsWith("/")) path += "index.htm";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
 // -------------------------------------------------------------------
 // Start Access Point, starts on 192.168.4.1
 // Access point is used for wifi network selection
@@ -357,6 +389,10 @@ void setup() {
   server.on("/reset", handleRst);
   server.on("/scan", handleScan);
   server.on("/apoff",handleAPOff);
+  server.onNotFound([](){
+  if(!handleFileRead(server.uri()))
+    server.send(404, "text/plain", "FileNotFound");
+  });
 	
 	server.begin();
 	Serial.println("HTTP server started");
