327c1220816cfa6cb29178c66edc4e0e3b4201fe
danbates2 <dgbates2@gmail.com>
2018-10-08 13:28:07 +0200

    commit

--- src/src.ino
+++ src/src.ino
@@ -1,27 +1,27 @@
 /*
-   -------------------------------------------------------------------
-   EmonESP Serial to Emoncms gateway
-   -------------------------------------------------------------------
-   Adaptation of Chris Howells OpenEVSE ESP Wifi
-   by Trystan Lea, Glyn Hudson, OpenEnergyMonitor
-   All adaptation GNU General Public License as below.
-
-   -------------------------------------------------------------------
-
-   This file is part of OpenEnergyMonitor.org project.
-   EmonESP is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3, or (at your option)
-   any later version.
-   EmonESP is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with EmonESP; see the file COPYING.  If not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-*/
+ * -------------------------------------------------------------------
+ * EmonESP Serial to Emoncms gateway
+ * -------------------------------------------------------------------
+ * Adaptation of Chris Howells OpenEVSE ESP Wifi
+ * by Trystan Lea, Glyn Hudson, OpenEnergyMonitor
+ * All adaptation GNU General Public License as below.
+ *
+ * -------------------------------------------------------------------
+ *
+ * This file is part of OpenEnergyMonitor.org project.
+ * EmonESP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * EmonESP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with EmonESP; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #include "emonesp.h"
 #include "config.h"
@@ -36,7 +36,7 @@
 // SETUP
 // -------------------------------------------------------------------
 void setup() {
-  delay(1000);
+  delay(2000);
 
   Serial.begin(115200);
 #ifdef DEBUG_SERIAL1
@@ -46,7 +46,7 @@ void setup() {
   DEBUG.println();
   DEBUG.print("EmonESP ");
   DEBUG.println(ESP.getChipId());
-  DEBUG.println("Firmware: " + currentfirmware);
+  DEBUG.println("Firmware: "+ currentfirmware);
 
   // Read saved settings from the config
   config_load_settings();
@@ -68,8 +68,8 @@ void setup() {
 // -------------------------------------------------------------------
 // LOOP
 // -------------------------------------------------------------------
-void loop() {
-
+void loop()
+{
   ota_loop();
   web_server_loop();
   wifi_loop();
@@ -79,16 +79,17 @@ void loop() {
 
   if (wifi_mode == WIFI_MODE_STA || wifi_mode == WIFI_MODE_AP_AND_STA)
   {
-    if (emoncms_apikey != 0 && gotInput) {
+    if(emoncms_apikey != 0 && gotInput) {
       emoncms_publish(input);
     }
-    if (mqtt_server != 0)
+    if(mqtt_server != 0)
     {
       mqtt_loop();
-      if (gotInput) {
+      if(gotInput) {
         mqtt_publish(input);
       }
     }
   }
 } // end loop
+
 
