c5cd7ac01c58b50af2ece75e2957adbb9eaaad21
Jeremy Poulter <jeremy@bigjungle.net>
2020-08-15 16:20:23 +0100

    Latest GUI

--- src/web_static/web_server_static_files.h
+++ src/web_static/web_server_static_files.h
@@ -6,6 +6,11 @@
 #include "web_server.style.css.h"
 #include "web_server.term.html.h"
 #include "web_server.term.js.h"
+#include "web_server.wifi_signal_1.svg.h"
+#include "web_server.wifi_signal_2.svg.h"
+#include "web_server.wifi_signal_3.svg.h"
+#include "web_server.wifi_signal_4.svg.h"
+#include "web_server.wifi_signal_5.svg.h"
 StaticFile staticFiles[] = {
   { "/assets.js", CONTENT_ASSETS_JS, sizeof(CONTENT_ASSETS_JS) - 1, _CONTENT_TYPE_JS },
   { "/config.js", CONTENT_CONFIG_JS, sizeof(CONTENT_CONFIG_JS) - 1, _CONTENT_TYPE_JS },
@@ -15,4 +20,9 @@ StaticFile staticFiles[] = {
   { "/style.css", CONTENT_STYLE_CSS, sizeof(CONTENT_STYLE_CSS) - 1, _CONTENT_TYPE_CSS },
   { "/term.html", CONTENT_TERM_HTML, sizeof(CONTENT_TERM_HTML) - 1, _CONTENT_TYPE_HTML },
   { "/term.js", CONTENT_TERM_JS, sizeof(CONTENT_TERM_JS) - 1, _CONTENT_TYPE_JS },
+  { "/wifi_signal_1.svg", CONTENT_WIFI_SIGNAL_1_SVG, sizeof(CONTENT_WIFI_SIGNAL_1_SVG) - 1, _CONTENT_TYPE_SVG },
+  { "/wifi_signal_2.svg", CONTENT_WIFI_SIGNAL_2_SVG, sizeof(CONTENT_WIFI_SIGNAL_2_SVG) - 1, _CONTENT_TYPE_SVG },
+  { "/wifi_signal_3.svg", CONTENT_WIFI_SIGNAL_3_SVG, sizeof(CONTENT_WIFI_SIGNAL_3_SVG) - 1, _CONTENT_TYPE_SVG },
+  { "/wifi_signal_4.svg", CONTENT_WIFI_SIGNAL_4_SVG, sizeof(CONTENT_WIFI_SIGNAL_4_SVG) - 1, _CONTENT_TYPE_SVG },
+  { "/wifi_signal_5.svg", CONTENT_WIFI_SIGNAL_5_SVG, sizeof(CONTENT_WIFI_SIGNAL_5_SVG) - 1, _CONTENT_TYPE_SVG },
 };
